b0c9c39b523b70477d55119e21fcb85ac58c0c20
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-27 10:35:28 +0530

    [Rupali]Feat: added stack implementation

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace stack_and_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            stack s = new stack();
+            int choice; int num;Boolean state = true;
+            while (state)
+            {
+
+
+                Console.WriteLine("enter your choice");
+                Console.WriteLine("1.push\n2.pop\n3.printStack\n4.Exit");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter data element");
+                        num = int.Parse(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case 2:
+                        s.pop();
+                        break;
+                    case 3:
+                        s.printStack();
+                        break;
+                    case 4:state = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("you enter wrong input");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stack_and_queue
+{
+    public class stack
+    {
+        int[] element = new int[5];
+        int top=-1;
+         int max=5;
+       
+
+        public void push(int item)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                
+            }
+            else
+            {
+                top++;
+               element [top] = item;
+            }
+        }
+
+        public void pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                
+            }
+            else
+            {
+                Console.WriteLine("{0} popped from stack ", element[top]);
+                top--;
+            }
+        }
+
+        
+
+        public void printStack()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                
+            }
+            else
+            {
+                for (int i = top; i>=0; i--)
+                {
+                    Console.WriteLine("{0} pushed into stack", element[i]);
+                }
+            }
+        }
+    }
+}
+
